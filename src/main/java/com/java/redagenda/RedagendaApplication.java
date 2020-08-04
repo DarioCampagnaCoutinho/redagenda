8c431f70bb61cde12e62dfbdbd5c647aa0e4a0d2
Dario Campagna <dario@localhost.localdomain>
2020-08-04 11:41:05 -0300

    Criação do projeto RedAgenda com Spring Boot

---
+++ src/main/java/com/java/redagenda/RedagendaApplication.java
@@ -0,0 +1,13 @@
+package com.java.redagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedagendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedagendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/redagenda/RedagendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.redagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedagendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
